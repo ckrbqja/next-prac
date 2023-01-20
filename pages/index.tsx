c717f0ce0f4f9952e3432dcb263d77e29ac42bef
chakyubeom <ckrbqja@gmail.com>
2023-01-20 16:09:44 +0900

    헤더

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,17 @@
 import type {NextPage} from 'next'
 import EventList from "../components/events/event-list";
 import {getAllEvents, getFeaturedEvents} from "../helper/api-utils";
+import Head from "next/head";
 
 const Home: NextPage = (props: any) => {
     // const featuredEvents = getFeaturedEvents() as any as EventItemType[];
 
 
     return <div>
+        <Head>
+            <title>NextJs Events</title>
+            <meta name="description" content="Find a log of great events that allow you to evolve..."/>
+        </Head>
         <EventList items={props.events}/>
     </div>
 }
